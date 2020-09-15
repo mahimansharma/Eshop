f6b9f525c6a9e82ecbde847666e4e8668dcf81fd
mahiman34 <mahiman.sharma34@gmail.com>
2020-09-15 17:58:51 +0530

    third  heroku

--- backend/index.js
+++ backend/index.js
@@ -41,6 +41,17 @@ app.use('',require('./routes/products'));
 // app.get('*', (req, res) => {
 //     res.sendFile(path.joinzx(`${__dirname}/../frontend/build/index.html`));
 // });
+
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static("frontend/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+    });
+}
+
+
 const port = process.env.PORT || 8000
 
 app.listen(port, () => {
