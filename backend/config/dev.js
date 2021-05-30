dea9852b84a90f5ad11c3def98459ba62ecc189e
Mahiman Sharma <mahiman.sharma@biz2credit.org>
2021-05-30 16:14:07 +0530

    config changes

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI : 'mongodb+srv://mahiman:Stillalive@cluster0-p69lf.mongodb.net/Eshop?retryWrites=true&w=majority'
+    mongoURI : 'mongodb+srv://mahiman:Test@1234@cluster0-p69lf.mongodb.net/Eshop?retryWrites=true&w=majority'
 }
